5fa4d7e9758a0104e873b19816ef38012575a8fa
Ousama Elayan <ousamasama@gmail.com>
2018-11-19 10:09:24 -0600

    finished practice/challenge

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Kennel from './components/Kennel';
+import './index.css';
+
+ReactDOM.render(<Kennel />, document.getElementById('root'));
